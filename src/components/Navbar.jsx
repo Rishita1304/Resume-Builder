661635c29324776e94b43fe98c410acff502e51e
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-08 00:00:24 +0530

    components built

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return(
+  <>
+    <div>
+        <ul className="flex justify-around h-20 items-center bg-gray-100 ">
+            <li>LOGO</li>
+            <li>Register</li>
+        </ul>
+    </div>
+  </>);
+};
+
+export default Navbar;
