20d0aacae42f87fc3855f29046d381acf2c44bfc
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-07 23:59:31 +0530

    src files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import CoverLetterForm from "./pages/CoverLetterForm";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/cover-letter" element={<CoverLetterForm />} />
+        </Routes>
+      </Router>
+      <div>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          draggable
+          pauseOnHover
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
